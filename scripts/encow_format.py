1d811464cb061985c333f384e421c00957978b7c
Luke Gessler <lukegessler@gmail.com>
2020-09-15 17:05:56 -0400

    encow format script

---
+++ scripts/encow_format.py
@@ -0,0 +1,17 @@
+import html
+with open('encow/encow16a01_1.sgml', 'r') as fin, open('encow/encow_sent.txt', 'w') as fout:
+    sent = []
+    for line in fin:
+        line = line.strip()
+        if len(line) == 0:
+            continue
+        if line[0] == '<' and line[-1] == '>':
+            if line[0:4] == "</s>":
+                fout.write(html.unescape(" ".join(sent) + "\n").replace("&bquo;", '"').replace("&equo;", '"'))
+                sent = []
+        else:
+            sent.append(line.split("\t")[0])
+
+import subprocess
+subprocess.run(["split", "-l", "100000", "--suffix-length=3", "-d", "encow_sent.txt", "encow_sent.txt."])
+print('OK')
